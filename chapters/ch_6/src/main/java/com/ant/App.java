1d596a0f87bd21bbaa3d641ecb85a80e11ae2ab6
mohamed.khalilsayed1 <mohamed.khalilsayed1@vodafone.com>
2025-06-26 16:39:31 +0300

    ch 6 ex initialization

---
+++ chapters/ch_6/src/main/java/com/ant/App.java
@@ -0,0 +1,17 @@
+package com.ant;
+
+import org.springframework.beans.factory.config.BeanDefinitionCustomizer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ chapters/ch_6/src/main/java/com/ant/ProjectConfig.java
@@ -0,0 +1,10 @@
+package com.ant;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.ant")
+public class ProjectConfig {
+
+}
